e137d0094dfdebda13a00b085571c8fb83fa5c52
Ethan <3149824805@qq.com>
2025-09-28 18:30:58 +0800

    "tri-common,alioss controller的实现"

--- tri-common/src/main/java/com/tri/common/CommonApplication.java
+++ tri-common/src/main/java/com/tri/common/CommonApplication.java
@@ -1,10 +1,12 @@
 package com.tri.common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tri.common.api.mapper")
 @Slf4j
 public class CommonApplication {
     public static void main(String[] args) {
---
+++ tri-common/src/main/java/com/tri/common/mapper/ImageRecordMapper.java
@@ -0,0 +1,23 @@
+package com.tri.common.mapper;
+
+
+import com.tri.common.pojo.entity.ImageRecord;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ImageRecordMapper {
+
+    @Insert("insert into tri_image_record(image_url) values(#{imageUrl})")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    Long insert(ImageRecord imageRecord);
+
+    @Select("select * from tri_image_record where id = #{id}")
+    ImageRecord selectById(Long id);
+
+    List<ImageRecord> selectByids(List<Long> ids);
+}
